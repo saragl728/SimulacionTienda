0be44fd54b8ea6e5573fa2dae3f1109d612848c8
saragl728 <saragl728@gmail.com>
2025-05-07 11:38:28 +0200

    Hago que la página de error redireccione a otra página y añado una imagen a un componente

--- src/app/componentes/error/error.component.ts
+++ src/app/componentes/error/error.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './error.component.css'
 })
 export class ErrorComponent {
+  readonly LIMITE : number = 1000 * 60 * 15;
+  
+  a = setTimeout(this.salta, this.LIMITE);  //después de que pase el tiempo se hará la redirección
+  
+  //función que dice cuándo se redirigirá al usuario desde que se cargó la página
+  tiempoRestante(){
+    let mens: string = "";
+    const MINUT = 60000;
+    const SEGU = 1000;
+
+    if (this.LIMITE < MINUT){
+      mens = (this.LIMITE / SEGU).toString() + $localize` segundo(s)`;
+    }
+    else{
+      if (this.LIMITE % MINUT == 0){
+        mens = (this.LIMITE / MINUT).toString() + $localize` minuto(s)`;
+      }
+      else {
+        mens = ((this.LIMITE - (this.LIMITE % MINUT)) / MINUT).toString() + $localize` minuto(s) y ` + ((this.LIMITE % MINUT) / SEGU).toString() + $localize` segundo(s)`;
+      }
+    }
+
+    return mens;
+  }
+
+  salta(){
+    location.replace('/mi-cuenta');
+  }
 
 }
